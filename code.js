9667d6135a8f44fee9b666afedb99fffc816fcf8
minj2 <minj2@hotmail.com>
2025-05-27 00:23:13 -0700

    HEX / RGB / HSL → OKLCH 변환

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-figma.showUI(__html__, { width: 320, height: 400 });
+figma.showUI(__html__, { width: 360, height: 280 });
 
-figma.ui.onmessage = msg => {
-  if (msg.type === 'convert-color') {
-    // 메시지 처리 로직 (예: 색상 선택 후 반영 등)
+figma.ui.onmessage = (msg) => {
+  if (msg.type === 'converted-oklch') {
+    figma.notify("Converted to OKLCH!");
   }
 };
